cccca7a18e62f871d34a2f7ef11369b145de76af
faithyoussef <38753427+faithyoussef@users.noreply.github.com>
2023-05-26 23:36:23 +0300

    Add files via upload

--- land-page-udacity/js/app.js
+++ land-page-udacity/js/app.js
@@ -31,27 +31,8 @@ let totalSections = 0;
 
 let customNavTimeout;
 
-// Start build the nav
-function buildNav(){
-    sections.forEach(section => {
-        //Create the li elements that contained inside the ul
-        const navButton = document.createElement('li');
-        //Insert the html text to  the li
-        navButton.insertAdjacentHTML("afterbegin",`<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
-        //Append the li to the ul
-        navList.appendChild(navButton);
-
-        //scrollBehavior Function Invoke
-        scrollBehavior(navButton, section);
-    });
-    //Append the ul to the nav
-    navBar.appendChild(navList);
-}
-
-//Build Nav Function Invoke
-buildNav();
-
-//End build the nav
+/* Helping Functions */
+//Result: It worked!
 const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
 
 //Return a random string
@@ -64,42 +45,9 @@ const returnRandomString = (numberOfTimes) => {
     return mainString;
 };
 
-
-// Start of Scroll to anchor ID using scrollTO event
-function scrollBehavior(navButton, section){
-    navButton.addEventListener('click', function(event){
-        event.preventDefault();
-        window.scrollTo({
-            top: section.offsetTop,
-            behavior:"smooth"
-        });
-    });
-}
-// End of Scroll to anchor ID using scrollTO event
-
-
-// Start of Set the Section class 'active' when it near to the top of viewport
-function activeSection (){
-    // Select all anchor using "menu__link" class
-    const navActive = document.querySelectorAll(".menu__link")
-    sections.forEach((section, i)=>{
-        //Get the boundingrect for each section 
-        const sectionBond = section.getBoundingClientRect();
-        //Check if the section is in viewport or not 
-        if (sectionBond.top <= 380 && sectionBond.bottom >= 350){
-            //section in viewport accourding to top and bottom boundings
-            //add 'your-active-class' class to the specific section
-            section.classList.add("your-active-class");
-            //add 'active_button' class to the specific nav button according to section ID
-            navActive[i].classList.add("active_button");
-        } else{
-            //Remove both section and navButton active classes when section is off sight
-            section.classList.remove("your-active-class");
-            navActive[i].classList.remove("active_button");
-        }
-    })
-}
-// End of Set the Section class 'active' when it near to the top of viewport
+//Source: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+//Create an observer for elements
+//Note: I converted this function into an arrow function as requested by the Udacity reviewer
 const createObserver = (elementToObserve) => {
     let options = {
         root: null,
@@ -142,42 +90,7 @@ const handleIntersect = (entries, observer) => {
     }
 };
 
-
-// Start of Toggle the NavBar According to User Scroll Activity
-function toggleNavBar(){
-    let userScroll;
-    //Default Settings for NavBar while scrolling
-    header.style.cssText = 'opacity: 1; transition: ease 0.3s ;'
-    // Cleartimeout throughout the scrolling
-    window.clearTimeout( userScroll );
-    //The Timeout to run after scrolling ends
-    userScroll = setTimeout(function() {
-        //The Settings Executed on NavBar after Timeout finished
-        header.style.cssText = 'opacity: 0; transition: ease 0.3s ;'
-    }, 6000);
-}
-// End of Toggle the NavBar According to User Scroll Activity
-
-
-//Start of the Scroll Event to execute the functions of activeSection and toggleNavBar 
-window.addEventListener('scroll',(event)=>{
-    activeSection();
-    toggleNavBar();
-})
-//End of the Scroll Event to execute the functions of activeSection and toggleNavBar 
-
-
-// Start of GO UP Button
-//Create the div element for the button 
-const goUpButton = footer.insertAdjacentHTML("beforebegin", `<div Id="return_top" ></div>`);
-// Scroll to top of the Landing Page using scrollTO event
-document.getElementById("return_top").addEventListener('click', function(){
-    window.scrollTo({
-        top: 0,
-        behavior:"smooth"
-    });
-});
-// End of GO UP Button
+/* Core Functions (and objects ;)) */
 
 const customNav = { //An object for handling custom navigation (dynamic, you say)
     navParentElement: document.querySelector('.nav'),
@@ -319,7 +232,7 @@ const main = () => {
     //This function is the thing that processes all the code we created, to actually make use of it.
 
     //Create the logo, use our emerald image and my... well.. name :)
-    customNav.createNavLogo('img/emerald-icon.png', 'faith\'s');
+    customNav.createNavLogo('img/emerald-icon.png', 'Abdelhady\'s');
 
     //Add our hamburger list at the beginning
     customNav.createNavHamburger();
